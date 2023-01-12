e73a957ecd7f73383b0df785f0e4c40946e7f3fd
Jchan <gkswocks123@gmail.com>
2023-01-12 13:18:55 +0900

    Feat : Install react-router-dom and set default

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import { AppRoutes } from "./pages/AppRoutes";
+
+/**
+ * @description Register all provider
+ */
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <AppRoutes />
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/AppRoutes.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Routes as ReactRoutes } from "react-router-dom";
+
+/**
+ * @description Register all routes
+ */
+import Main from "./Main";
+
+export const AppRoutes = () => {
+  return (
+    <ReactRoutes>
+      <Route path="/" element={<Main />} />
+    </ReactRoutes>
+  );
+};
